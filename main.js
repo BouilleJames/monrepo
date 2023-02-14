3d9ca899b63960fa166216fa82284227c5af1799
BouilleJames <77192627+BouilleJames@users.noreply.github.com>
2023-02-14 11:02:38 +0100

    update

---
+++ main.js
@@ -0,0 +1,33 @@
+const prenom = "Sofiane";
+console.log(typeof (prenom));
+const nom = "Marley";
+const age = 36;
+const phrase = " Bonjour , je m'appelle";
+window.alert (phrase + " " + prenom+  " "  +  " "  + nom +  " " + " et  j'ai " + age + " ans " );
+var monTableau = [];
+monTableau["enfant"]= "toto";
+monTableau["maman"]= "tata";
+monTableau["papa"]= "titi";
+console.table (monTableau);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
